700f6f0f403b5502b0ff6b699e2874ecc4d24296
RudenokArt <rudenokart@yandex.ru>
2022-10-30 10:41:09 +0300

    conflict mergin

--- modular/connectdb.php
+++ modular/connectdb.php
@@ -2,13 +2,13 @@
 
 $db_host = "localhost"; // Подключаемся к базе
 
-// $db_user = "root"; // Логин БД
-// $db_password = "root"; // Пароль БД
-// $db_base = 'cx57370_modul'; // Имя БД
-
-$db_user = "cx57370_modul"; // Логин БД
-$db_password = "cx57370"; // Пароль БД
+$db_user = "root"; // Логин БД
+$db_password = "root"; // Пароль БД
 $db_base = 'cx57370_modul'; // Имя БД
+
+// $db_user = "cx57370_modul"; // Логин БД
+// $db_password = "cx57370"; // Пароль БД
+// $db_base = 'cx57370_modul'; // Имя БД
 
 $mysqli = new mysqli($db_host,$db_user,$db_password,$db_base);
 if ($mysqli->connect_error) 
