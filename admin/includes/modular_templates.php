50af89925f9feb2cbbe1b6dd0df7ad108b063e99
RudenokArt <rudenokart@yandex.ru>
2022-11-04 17:59:02 +0300

    save

--- admin/includes/modular_templates.php
+++ admin/includes/modular_templates.php
@@ -1,3 +1,14 @@
+
+<?php if ($_POST['modular_template_sort']) {
+  $modular_admin->dbQuery('UPDATE `constractor_templates`
+    SET 
+    `price`='.$_POST['modular_template_sort'].'
+    WHERE `id`='.$_POST['id']);
+  // echo '<meta http-equiv="refresh" content="0; url=?tab=modular_templates" />';
+} ?>
+
+<pre><?php print_r($_POST) ?></pre>
+
 <div class="container">
   <div class="row pt-5">
     <a href="#" class="btn btn-outline-primary">
@@ -23,16 +34,16 @@
               Сортировка:
             </div>
           </div>
-          <div class="row pb-2">
+          <form class="row pb-2" action="" method="post">
             <div class="col">
-              <input type="text" class="form-control" value="<?php echo $value['price'] ?>">              
+              <input type="text" name="modular_template_sort" class="form-control" value="<?php echo $value['price'] ?>">              
             </div>
             <div class="col">
-              <button title="сохранить" class="btn btn-outline-primary h-100">
+              <button name="id" value="<?php echo $value['id'] ?>" title="сохранить" class="btn btn-outline-primary h-100">
                 <i class="fa fa-floppy-o" aria-hidden="true"></i>
               </button>
             </div>
-          </div>
+          </form>
           <div class="row border-top pt-2 pb-1">
            <a href="#" class="btn btn-outline-danger">
              <i class="fa fa-trash-o" aria-hidden="true"></i>
